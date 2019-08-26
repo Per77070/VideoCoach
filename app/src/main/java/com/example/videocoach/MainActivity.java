63fcbcd621e737612699b30de3d2d4de68f022f9
Per77070 <per.gustafsson77@outlook.com>
2019-08-26 15:41:33 +0200

    First commit

---
+++ app/src/main/java/com/example/videocoach/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.videocoach;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+    }
+
+
+
+}
+
+
